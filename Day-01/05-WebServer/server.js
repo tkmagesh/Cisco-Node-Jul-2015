56b35e3dd72eb2ec4a1a006878830b928baeb493
tkmagesh <tkmagesh77@gmail.com>
2015-07-01 16:10:59 +0530

    app server - with post

--- Day-01/05-WebServer/server.js
+++ Day-01/05-WebServer/server.js
@@ -24,13 +24,31 @@ var server = http.createServer(function(req, res){
         }
         fs.createReadStream(resourcePath).pipe(res);
     } else if (req.url.pathname === "/calculator"){
-      var n1 = parseInt(req.query.n1,10),
-          n2 = parseInt(req.query.n2,10),
-          operation = req.query.operation;
+        if (req.method === "GET"){
+          var n1 = parseInt(req.query.n1,10),
+              n2 = parseInt(req.query.n2,10),
+              operation = req.query.operation;
 
-      var result = calculator[operation](n1, n2);
-      res.write(result.toString());
-      res.end();
+          var result = calculator[operation](n1, n2);
+          res.write(result.toString());
+          res.end();
+        } else {
+            var data = '';
+            req.on('data', function(chunk){
+                data += chunk;
+            });
+            req.on('end', function(){
+                req.body = qs.parse(data);
+                 var n1 = parseInt(req.body.n1,10),
+                      n2 = parseInt(req.body.n2,10),
+                      operation = req.body.operation;
+
+                  var result = calculator[operation](n1, n2);
+                  res.write(result.toString());
+                  res.end();
+            });
+
+        }
     } else {
         res.statusCode = 404;
         res.end();
