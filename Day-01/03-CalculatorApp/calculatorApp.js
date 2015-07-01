54bc1ea0588856b410023a809979fab3559805dc
tkmagesh <tkmagesh77@gmail.com>
2015-07-01 11:52:29 +0530

    Simple Demos

---
+++ Day-01/03-CalculatorApp/calculatorApp.js
@@ -0,0 +1,7 @@
+var calculator = require("./calculator");
+console.log("Processing 100 & 200")
+var x = 100, y = 200;
+console.log("add = ", calculator.add(x,y))
+console.log("subtract = ", calculator.subtract(x,y))
+console.log("multiply = ", calculator.multiply(x,y))
+console.log("divide = ", calculator.divide(x,y))
---
+++ Day-01/04-CalculatorApp-Assignment/calculatorApp.js
@@ -0,0 +1,6 @@
+var calculator = require("./calculator");
+calculator.add(10);
+calculator.subtract(20);
+calculator.multiply(3);
+calculator.divide(5);
+console.log(calculator.getResult());
