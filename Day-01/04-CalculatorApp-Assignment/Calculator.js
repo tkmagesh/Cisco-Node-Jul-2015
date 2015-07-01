cf97accda0bc790a533cb9294b679c90252db95e
tkmagesh <tkmagesh77@gmail.com>
2015-07-01 12:21:32 +0530

    Constructor Function

---
+++ Day-01/04-CalculatorApp-Assignment/Calculator.js
@@ -0,0 +1,19 @@
+function Calculator(){
+    var result = 0;
+    this.add = function(x){
+        result += x;
+    };
+    this.subtract = function(x){
+        result -= x;
+    };
+    this.multiply = function(x){
+        result *= x;
+    };
+    this.divide = function(x){
+        result /= x;
+    };
+    this.getResult = function(){
+        return result;
+    }
+}
+module.exports = Calculator;
---
+++ Day-01/04-CalculatorApp-Assignment/getCalculator.js
@@ -0,0 +1,23 @@
+function getCalculator(){
+    var result = 0;
+    var calculator = {
+        add : function(x){
+            result += x;
+        },
+        subtract : function(x){
+            result -= x;
+        },
+        multiply : function(x){
+            result *= x;
+        },
+        divide : function(x){
+            result /= x;
+        },
+        getResult : function(){
+            return result;
+        }
+    };
+    return calculator;
+}
+module.exports = getCalculator;
+
